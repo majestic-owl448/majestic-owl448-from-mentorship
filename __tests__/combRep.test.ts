93149db4172eb247d4a36d4a90601cd623b90030
Ilenia M. <26656284+majestic-owl448@users.noreply.github.com>
2026-08-27 11:56:57 +0200

    test: isolate database and cover combination helper

--- __tests__/combRep.test.ts
+++ __tests__/combRep.test.ts
@@ -1,5 +1,22 @@
 import combRep from "@/lib/combRep";
 
 describe("combRep", () => {
-  // Tests will be added here
+  it("uses the input length when no combination length is provided", () => {
+    expect(combRep(["a", "b"])).toEqual([
+      ["a", "a"],
+      ["a", "b"],
+      ["b", "b"],
+    ]);
+  });
+
+  it("returns combinations with repetition for an explicit length", () => {
+    expect(combRep([1, 2, 3], 2)).toEqual([
+      [1, 1],
+      [1, 2],
+      [1, 3],
+      [2, 2],
+      [2, 3],
+      [3, 3],
+    ]);
+  });
 });
